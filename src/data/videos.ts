b70065e34b49fffcc464bc3b2d7849cf3cdb7875
edesgree <edouard.desgree@gmail.com>
2026-06-01 12:38:51 +0200

    clean images path

--- src/data/videos.ts
+++ src/data/videos.ts
@@ -19,7 +19,7 @@ export const videos: Video[] = [
     year: '2020',
     category: 'Music Video',
     embedUrl: 'https://www.youtube.com/embed/S1b5jhz_IYc?si=zwbr0Qyr4WN7Rsfz',
-    thumbnail: '/images/videos/project-one.jpg',
+    thumbnail: '/images/videos/project-one.webp',
     description: `Pastel is a 28-year-old french/Polynesian self-taught multi-instrumentalist, songwriter and producer based in London.
     His introduction to music came at just 10 years old and took shape when he got a touch on his first keyboard, guitar and bass, which he learnt to record on his grandpa’s tape machine, recording his earliest creations. Next, he began booking studio sessions to record his band demos while delving into the world of recording his own songs.
     The past four years have seen Pastel tour in Asia four times, building his audience over there. His support for FKJ and collaborations with artists like Bazzi, Bon Entendeur, Adeline and Jarreau Vandal made his name stand out in the world of neo-soul and indie pop jazz scene.
@@ -36,7 +36,7 @@ export const videos: Video[] = [
     year: '2019',
     category: 'Music Video',
     embedUrl: 'https://www.youtube.com/embed/ogn1rt67Ewk?si=kE32F8jWcvBDi2N2',
-    thumbnail: '/images/videos/project-two.jpg',
+    thumbnail: '/images/videos/project-two.webp',
     description: `Will Purdue is a British Alternative Country-Rock singer-songwriter inspired by the likes of Leonard Cohen, The National, Drive-by Truckers and Damien Dempsey. His sound is characterised by his always-present acoustic guitar and his deep voice, smooth and smoky on his ballads and rocky and poignant on his upbeat songs.
     The video is conducted like a short documentary showing the life of a performance artist & drag queen Stephen Eyre alias Cassandra.`,
     credits: [
